e8d98eaee347f56a86825cac789e681baf985f53
Hendi Santika <hendisantika@yahoo.co.id>
2021-01-03 06:18:31 +0700

    #9 - Add UserAccountRepository

---
+++ src/main/java/com/hendisantika/springbootsecuritysessionredis/repository/UserAccountRepository.java
@@ -0,0 +1,20 @@
+package com.hendisantika.springbootsecuritysessionredis.repository;
+
+import com.hendisantika.springbootsecuritysessionredis.entity.UserAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-security-session-redis
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 03/01/21
+ * Time: 06.17
+ */
+@Repository
+public interface UserAccountRepository extends JpaRepository<UserAccount, Integer> {
+
+    UserAccount findByUsername(String username);
+}
