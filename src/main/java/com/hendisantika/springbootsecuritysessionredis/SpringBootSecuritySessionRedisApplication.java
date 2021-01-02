2b68f18f81f1eb34990ccefa4499502802f779d9
Hendi Santika <hendisantika@yahoo.co.id>
2021-01-03 06:06:30 +0700

    #1 - First commit

---
+++ src/main/java/com/hendisantika/springbootsecuritysessionredis/SpringBootSecuritySessionRedisApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootsecuritysessionredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecuritySessionRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecuritySessionRedisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootsecuritysessionredis/SpringBootSecuritySessionRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootsecuritysessionredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecuritySessionRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
