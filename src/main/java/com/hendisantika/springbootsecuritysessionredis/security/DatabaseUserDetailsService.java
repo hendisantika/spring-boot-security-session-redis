ef459eb5d05f1191b74f10798c22bd641544b224
Hendi Santika <hendisantika@yahoo.co.id>
2021-01-03 06:22:54 +0700

    #14 - Add loadUserByUsername in DatabaseUserDetailsService

--- src/main/java/com/hendisantika/springbootsecuritysessionredis/security/DatabaseUserDetailsService.java
+++ src/main/java/com/hendisantika/springbootsecuritysessionredis/security/DatabaseUserDetailsService.java
@@ -1,8 +1,19 @@
 package com.hendisantika.springbootsecuritysessionredis.security;
 
+import com.hendisantika.springbootsecuritysessionredis.entity.UserAccount;
+import com.hendisantika.springbootsecuritysessionredis.entity.UserRoleToPrivilege;
+import com.hendisantika.springbootsecuritysessionredis.entity.UserToRole;
 import com.hendisantika.springbootsecuritysessionredis.repository.UserAccountRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,5 +32,26 @@ public class DatabaseUserDetailsService implements UserDetailsService {
 
     public DatabaseUserDetailsService(UserAccountRepository userAccountRepository) {
         this.userAccountRepository = userAccountRepository;
+    }
+
+    @Transactional
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserAccount userAccount = userAccountRepository.findByUsername(username);
+        if (userAccount == null) {
+            throw new UsernameNotFoundException("User with username [" + username + "] not found in the system");
+        }
+
+        Set<GrantedAuthority> authorities = new HashSet<>();
+
+        for (UserToRole userToRole : userAccount.getUserToRoles()) {
+            authorities.add(new SimpleGrantedAuthority("ROLE_" + userToRole.getRole().getRoleName()));
+            for (UserRoleToPrivilege userRoleToPrivilege : userToRole.getRole().getUserRoleToPrivileges()) {
+                authorities.add(new SimpleGrantedAuthority(userRoleToPrivilege.getPrivilege().getPrivilegeName()));
+            }
+        }
+
+        return new CustomUserDetails(userAccount.getUsername(), userAccount.getPassword(), userAccount.isActive(),
+                authorities);
     }
 }
